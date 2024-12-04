5adc3ee790b1a094873d089310e905af8d15a709
PrimeMinisterSG <184567941+PrimeMinisterSG@users.noreply.github.com>
2024-12-04 13:01:25 +0800

    Changed to work with DOM manipulation

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+let result = "";
 
 function getComputerChoice () {
     const computerChoice = Math.floor((Math.random()*3)+1);
@@ -10,45 +13,73 @@ function getComputerChoice () {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper or Scissors? Type one into the box");
-    return humanChoice.toUpperCase()       
-    }
-
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice.toUpperCase()) {
-        console.log("It's a draw!");
+        result = "It's a draw!";
     } else if (humanChoice==="ROCK" && computerChoice==="Paper") {
-                console.log("You lose! Paper beats rock.");
+                result = "You lose! Paper beats rock.";
                 return computerScore++
     } else if (humanChoice==="PAPER" && computerChoice==="Scissors") {
-        console.log("You lose! Scissors beats paper.");
+        result = "You lose! Scissors beats paper.";
         return computerScore++
     } else if (humanChoice==="SCISSORS" && computerChoice==="Rock") {
-        console.log("You lose! Rock beats Scissors.");
+        result ="You lose! Rock beats Scissors.";
         return computerScore++
     } else {
-        console.log("Human wins! Death to robots!")
+        result = "Human wins! Death to robots!"
         humanScore++;
-        console.log(humanScore)
+        } 
+}
+
+function displayScore (humanS, computerS) {
+    const writeScore = document.createElement("li");
+    if (humanS ===5) {
+        writeScore.textContent = "Game over! You won! Click one of the buttons to start over"
+        list.appendChild(writeScore);
+        humanScore = 0;
+        computerScore = 0;
+        //console.log(humanScore)
+    } else  if (computerS ===5) {
+        writeScore.textContent = "Game over! You lost! Click one of the buttons to start over"
+        list.appendChild(writeScore);
+        humanScore = 0;
+        computerScore = 0;
+    } else if (humanS || computerS <5) {
+    writeScore.textContent = result + " Your score is: " + humanScore + ", computer score is: " + computerScore;
+    list.appendChild(writeScore)
     }
 }
+
+
+const list = document.querySelector('ul');
+const rockBtn = document.querySelector('#rockButton');
+const paperBtn = document.querySelector('#paperButton');
+const scissorsBtn = document.querySelector('#scissorsButton');
 
 function playGame() {
-    for (let i = 1; i < 6; i++) {
-    var human = getHumanChoice();
-    var computer = getComputerChoice();
 
-        playRound(human, computer);
-        console.log ("Round " + i + "/5");
-        console.log ("Human score = "+ humanScore+", Skynet score = " + computerScore);
-    }
-    if (computerScore>humanScore) {
-        console.log("You lost. The robots have won. Judgment Day Is Inevitable.")
-    } else {
-        console.log("Congratulations you won! The Future Has Not Been Written. There Is No Fate But What We Make For Ourselves.")
-    }
-}
-var humanScore = 0;
-var computerScore = 0;
+rockBtn.addEventListener('click', () => {
+    var human = "ROCK";
+    list.removeChild(list.firstChild);
+    console.log(humanScore)
+    playRound(human, getComputerChoice())
+    displayScore(humanScore,computerScore)
+})
+
+paperBtn.addEventListener('click', () => {
+    var human = "PAPER";
+    list.removeChild(list.firstChild);
+    playRound(human, getComputerChoice());
+    displayScore(humanScore,computerScore)
+})
+
+scissorsBtn.addEventListener('click', () => {
+    var human = "SCISSORS";
+    list.removeChild(list.firstChild);
+    playRound(human, getComputerChoice())
+    displayScore(humanScore,computerScore)
+})
+} 
+
+
 playGame()
